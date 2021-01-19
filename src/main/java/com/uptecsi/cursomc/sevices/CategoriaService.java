3409970fc5e8eeae7555ad9b7e5deaed988dbda2
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-01-19 15:41:06 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/uptecsi/cursomc/sevices/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.uptecsi.cursomc.sevices;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uptecsi.cursomc.domain.Categoria;
+import com.uptecsi.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);		
+	}
+}
