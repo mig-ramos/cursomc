36a108d1e0ff92c7b8b3743f219af80ac104263a
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-01-19 15:54:14 -0300

    Criando operacao de instanciacao

--- src/main/java/com/uptecsi/cursomc/CursomcApplication.java
+++ src/main/java/com/uptecsi/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.uptecsi.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uptecsi.cursomc.domain.Categoria;
+import com.uptecsi.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepositiry;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escrotótio");
+		
+		categoriaRepositiry.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
